e11ce6467b46d00554e1647f86ac280ce5a4ff24
wuzhenji <15195035689@163.com>
2022-01-10 19:54:40 +0800

    配置自动打包

--- main.js
+++ main.js
@@ -1,16 +1,45 @@
-const { app, BrowserWindow } = require('electron')
-const isDev = require('electron-is-dev')
-const path = require("path")
-let mainWindow
+const { app, BrowserWindow, dialog } = require('electron');
+const isDev = require('electron-is-dev');
+const path = require('path');
+const { autoUpdater } = require('electron-updater');
+let mainWindow;
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({
-        width: 1024,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
+  autoUpdater.autoDownload = false;
+  autoUpdater.checkForUpdatesAndNotify();
+  autoUpdater.on('error', (error) => {
+    dialog.showErrorBox('Error: ', error);
+  });
+  autoUpdater.on('update-available', () => {
+    dialog.showMessageBox(
+      {
+        type: 'info',
+        title: '应用有新的版本',
+        message: '发现新版本，是否现在更新',
+        buttons: ['是', '否'],
+      },
+      (buttonIndex) => {
+        if (buttonIndex === 0) {
+          autoUpdater.downloadUpdate();
         }
-    })
-    const urlLocation = isDev ? 'http://localhost:8000' : `file://${path.join(__dirname, './build/index.html')}`
-    mainWindow.loadURL(urlLocation)
-})
+      },
+    );
+  });
+  autoUpdater.on('update-not-available', () => {
+    dialog.showMessageBox({
+      title: '没有新版本',
+      message: '当前已经是最新版本',
+    });
+  });
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  const urlLocation = isDev
+    ? 'http://localhost:8000'
+    : `file://${path.join(__dirname, './build/index.html')}`;
+  mainWindow.loadURL(urlLocation);
+});
